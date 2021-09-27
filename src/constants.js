2aaaf40fb9e60848426897e0af786a1c5a6be24a
Takiyo <mail@chilledchino.live>
2021-09-27 21:15:35 +0700

    add constant

---
+++ src/constants.js
@@ -0,0 +1,38 @@
+const kazagumoTrack = require('./kazagumoTrack');
+
+/**
+ * Statics for some constants
+ * @class constant
+ */
+module.exports = {
+    /**
+     * Options for kazagumo
+     * @property {Object} [spotify={}] Spotify configs
+     * @property {string} spotify.clientId Spotify client ID
+     * @property {string} spotify.clientSecret Spotify client Secret
+     * @property {("youtube"|"youtube_music"|"soundcloud")} [defaultSearchEngine="youtube"] Search engine for resolving and search
+     * @memberOf constant
+     */
+    kazagumoOptions: {
+        spotify: {
+            clientId: null,
+            clientSecret: null
+        },
+        defaultSearchEngine: null
+    },
+
+    /**
+     * Kagazumo's search result
+     * @property {number} selectedTrack
+     * @property {("PLAYLIST"|"TRACK"|"SEARCH"|null)} type Result's type
+     * @property {void[]|kazagumoTrack[]} tracks Result's tracks
+     * @property {null|string} playlistName Playlist's name if it's a playlist
+     * @memberOf constant
+     */
+    searchResult: {
+        selectedTrack: 0,
+        type: null,
+        tracks: [],
+        playlistName: ""
+    }
+}
