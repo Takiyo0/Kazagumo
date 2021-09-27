d07f15ea912ab1c70da918e5948195eae2c84eb8
Takiyo <mail@chilledchino.live>
2021-09-27 13:14:59 +0700

    add utils

---
+++ src/kazagumoUtils.js
@@ -0,0 +1,19 @@
+module.exports = {
+    /**
+     * Move an item on array to a specific index
+     * @param {Array} arr Main array
+     * @param {number} old_index Old index
+     * @param {number} new_index New index
+     * @returns {*}
+     */
+    moveArray: (arr, old_index, new_index) => {
+        if (new_index >= arr.length) {
+            let k = new_index - arr.length + 1;
+            while (k--) {
+                arr.push(undefined);
+            }
+        }
+        arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+        return arr;
+    }
+}
