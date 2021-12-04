1eaff981c252209c74ce87aa2edd0aa43aa4e14a
Takiyo <alanwalkerlol049@gmail.com>
2021-12-04 15:40:07 +0700

    the previous fix didn't actually fix the issue

--- src/kazagumoTrack.js
+++ src/kazagumoTrack.js
@@ -46,15 +46,14 @@ class kazagumoTrack {
         this.uri = kazagumoRawTrack.info.uri;
         this.realUri = this.checkSupportedSource() ? kazagumoRawTrack.info.uri : null;
         this.requester = requester || null;
-        
-        console.log(`https://img.youtube.com/vi/${this.identifier}/${this.kazagumo?._kazagumoOptions?.defaultThumbnail || "maxresdefault"}.jpg okok `, this.thumbnail);
-        if (this.kazagumo?._kazagumoOptions?.defaultThumbnail && this.identifier && this.sourceName === "youtube") this.thumbnail = `https://img.youtube.com/vi/${this.identifier}/${this.kazagumo?._kazagumoOptions?.defaultThumbnail || "maxresdefault"}.jpg`;
-        console.log(`https://img.youtube.com/vi/${this.identifier}/${this.kazagumo?._kazagumoOptions?.defaultThumbnail || "maxresdefault"}.jpg okok `, this.thumbnail);
-        
+                
         /**
          * @private
          */
         this.kazagumo = kazagumo;
+        
+        if (this.kazagumo?._kazagumoOptions?.defaultThumbnail && this.identifier && this.sourceName === "youtube") this.thumbnail = `https://img.youtube.com/vi/${this.identifier}/${this.kazagumo?._kazagumoOptions?.defaultThumbnail || "maxresdefault"}.jpg`;
+
         /**
          * Whether the track has been resolved automatically when the track's source is in resolveSource option
          * @private
