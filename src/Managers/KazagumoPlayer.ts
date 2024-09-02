8d6a67d8bc6c70079c10524f078e4761cee8dae8
Takiyo Takahashi <takiyotakahashi@gmail.com>
2024-09-02 21:59:35 +0700

    fix queueUpdate throwing typing error with shoukaku 4.1.0

--- src/Managers/KazagumoPlayer.ts
+++ src/Managers/KazagumoPlayer.ts
@@ -28,7 +28,7 @@ export class KazagumoPlayer {
   /**
    * Kazagumo Instance
    */
-  private readonly kazagumo: Kazagumo;
+  public readonly kazagumo: Kazagumo;
   /**
    * Shoukaku's Player instance
    */
@@ -167,6 +167,10 @@ export class KazagumoPlayer {
     this.shoukaku.on('update', (data: PlayerUpdate) => this.emit(Events.PlayerUpdate, this, data));
     this.shoukaku.on('stuck', (data: TrackStuckEvent) => this.emit(Events.PlayerStuck, this, data));
     this.shoukaku.on('resumed', () => this.emit(Events.PlayerResumed, this));
+    // @ts-ignore
+    this.shoukaku.on(Events.QueueUpdate, (player: KazagumoPlayer, queue: KazagumoQueue) =>
+      this.kazagumo.emit(Events.QueueUpdate, player, queue),
+    );
   }
 
   // /**
