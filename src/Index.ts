03390abfde2b85690f56ca4c22b39f2db09278d4
takiyo <takiyotakahashi@icloud.com>
2025-11-10 21:23:00 +0700

    chore: bump version to 3.4.0 and update dependencies

--- src/Index.ts
+++ src/Index.ts
@@ -11,4 +11,4 @@ export * from './Kazagumo';
 export { KazagumoTrack, KazagumoQueue, KazagumoPlayer, Plugins };
 export * from './Modules/Interfaces';
 
-export const version = '3.3.0';
+export const version = '3.4.0';
